1e21b70de4dce13c29d3df6ff9c515309862293f
Dan Finnie <dan@danfinnie.com>
2015-04-15 15:30:59 -0400

    Add day of week, fix off by one error in date distance calculator

--- lib/column_3_problem_4.rb
+++ lib/column_3_problem_4.rb
@@ -15,15 +15,27 @@ class Column3Problem4
     31
   ]
 
+  MONDAY = 0
+  TUESDAY = 1
+  WEDNESDAY = 2
+  THURSDAY = 3
+  FRIDAY = 4
+  SATURDAY = 5
+  SUNDAY = 6
+
   def date_distance(y1, m1, d1, y2, m2, d2)
     x1 = days_since_start_of_time(y1, m1, d1)
     x2 = days_since_start_of_time(y2, m2, d2)
     x2 - x1
   end
 
+  def day_of_week(y, m, d)
+    date_distance(2015, 4, 13, y, m, d) % 7
+  end
+
   private
 
   def days_since_start_of_time(y, m, d)
-    y * 365 + MONTH_LENGTHS[0...m].inject(&:+) + d
+    y * 365 + MONTH_LENGTHS[0...(m-1)].inject(0, &:+) + d
   end
 end
--- spec/column_3_problem_4_spec.rb
+++ spec/column_3_problem_4_spec.rb
@@ -12,8 +12,22 @@ RSpec.describe "Column 3" do
         expect(thingy.date_distance(2014, 12, 30, 2015, 1, 2)).to eq(3)
       end
 
+      it "works for month boundaries" do
+        expect(thingy.date_distance(2015, 4, 13, 2015, 5, 28)).to eq(45)
+      end
+
       it "works with dates seperated by more than a year" do
         expect(thingy.date_distance(2014, 12, 30, 2016, 1, 2)).to eq(3+365)
+      end
+    end
+
+    describe "#day_of_week" do
+      it "works for tax day" do
+        expect(thingy.day_of_week(2015, 4, 15)).to eq(Column3Problem4::WEDNESDAY)
+      end
+
+      it "works for may 28 2015" do
+        expect(thingy.day_of_week(2015, 5, 28)).to eq(Column3Problem4::THURSDAY)
       end
     end
   end
